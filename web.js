767cd07f1146b3c9fdc9b0b8d15bb1fd2f1b39fa
Panagiotis Ntafopoulos <p.ntafopoulos@gmail.com>
2013-07-12 11:34:25 +0000

    updated web.js with link to index.html

--- web.js
+++ web.js
@@ -4,16 +4,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-fs.readFileSync("index.html");
+fs.readFileSync('index.html');
 
-var buff = new Buffer(Buffer.bytelength(fs));
+var buff = new Buffer();
 
-buff.write(fs);
+var txt = buff.toString('utf-8');
 
 
 app.get('/', function(request, response) {
 	//response.send('Hello World2!');
-	responce.send(buff.toString());
+	responce.send(txt);
 });
 
 var port = process.env.PORT || 5000;
