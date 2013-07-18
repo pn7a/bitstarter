5ab71e77a2b8380f4bf0e462d03e4d759ae955dc
Panagiotis Ntafopoulos <p.ntafopoulos@gmail.com>
2013-07-18 07:32:07 +0000

    updated grader

--- grader.js
+++ grader.js
@@ -24,8 +24,15 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var html = "";
+
+var assertURL = function(url) {
+    var url = url.toString();
+    rest.get(url).on('complete', setHtml);
+}
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,14 +68,45 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var checkHtml = function(html, checksfile) {
+   $ = cheerio.load(html);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
+var checkAndOut = function (html, checks) {
+    var checkJSON = checkHtml(html, checks);
+    var outJSON = JSON.stringify(checkJSON, null, 4);
+    console.log(outJSON);
+}
+
 if(require.main == module) {
+
+    var setHtml = function(result, response) {
+	if (result instanceof Error) {
+	    console.error('Error: ' + result.message);
+	    process.exit(1);
+	} else {
+	    html = result;
+	    checkAndOut(html, program.checks);
+	}
+    };
+
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to index.html', clone(assertURL))
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (! program.url) {
+	html = fs.readFileSync(program.file);
+	checkAndOut(html, program.checks);
+    }
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
